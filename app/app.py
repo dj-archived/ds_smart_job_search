9dfb59918a4f30ccbba676d8401d05dbfe4a68b7
dujm <jdu523@outlook.com>
2019-04-25 15:12:35 +0200

    black py

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from exceptions import ImproperlyConfigured
 
-pd.set_option('display.max_columns', None)
+pd.set_option("display.max_columns", None)
 ################################################################################
 if "DYNO" in os.environ:
     # the app is on Heroku
@@ -44,28 +44,30 @@ external_css = [
 theme = {"font-family": "Lobster", "background-color": "#e0e0e0"}
 ################################################################################
 # Read data
-#df =pd.read_csv('./data/Global-Artificial-Intelligence-Database-Asgard-2018.csv',encoding = "ISO-8859-1")
+# df =pd.read_csv('./data/Global-Artificial-Intelligence-Database-Asgard-2018.csv',encoding = "ISO-8859-1")
 
-#df = df.drop('Description', 1)
+# df = df.drop('Description', 1)
 # add index
-#df[' index'] = range(1, len(df) + 1)
+# df[' index'] = range(1, len(df) + 1)
 
 # Filter healthcare
-#df_health = df[df['Category'].str.contains("Health")]
-#df_health = df.loc[df.Category.str.contains('Health', na=False)]
-df_health =pd.read_csv('./data/df_health.csv',encoding = "ISO-8859-1")
+# df_health = df[df['Category'].str.contains("Health")]
+# df_health = df.loc[df.Category.str.contains('Health', na=False)]
+df_health = pd.read_csv("./data/df_health.csv", encoding="ISO-8859-1")
 #####################################################################
-df1 =pd.read_csv('./data/de/Indeed_Germany.csv')
+df1 = pd.read_csv("./data/de/Indeed_Germany.csv")
 df1.head(2)
-df1['Number'] = range(1, len(df1) + 1)
+df1["Number"] = range(1, len(df1) + 1)
 
 #####################################################################
-#df_health.to_csv('./data/df_health.csv')
+# df_health.to_csv('./data/df_health.csv')
 ################################################################################
 def create_header():
     header_style = {"background-color": theme["background-color"], "padding": "1.5rem"}
     header = html.Header(html.H1(children=app_name, style=header_style))
     return header
+
+
 #########################
 def create_content():
     content = html.Div(
@@ -74,7 +76,8 @@ def create_content():
             html.Hr(),
             html.Div(
                 children=[
-                    dcc.Markdown("""
+                    dcc.Markdown(
+                        """
                         Top Countries in the Race for Artificial Intelligence
 
                         ![AI](https://asgard.vc/wp-content/uploads/2018/05/Global-Artificial-Intelligence-Landscape-Industry-Map-International-by-Asgard-Capital-2018-and-Roland-Berger-1024x678.jpg)
@@ -87,17 +90,26 @@ def create_content():
                         > Berlin, Toronto, Shenzhen, and Seoul follow closely.
                         >
                         > Applied AI Solutions Aren’t Deep-Tech Enough
-                        """.format(number="thousand").replace("  ", ""))],
+                        """.format(
+                            number="thousand"
+                        ).replace(
+                            "  ", ""
+                        )
+                    )
+                ],
                 className="row",
                 style={"margin-bottom": 20},
-                        ),
+            ),
             html.Hr(),
             html.H2("Health AI companies"),
             html.Div(
                 children=[
                     dash_table.DataTable(
-                        id='datatable-interactivity2',
-                        columns=[{"name": i, "id": i, "deletable": True} for i in df_health.columns],
+                        id="datatable-interactivity2",
+                        columns=[
+                            {"name": i, "id": i, "deletable": True}
+                            for i in df_health.columns
+                        ],
                         data=df_health.to_dict("rows"),
                         editable=True,
                         filtering=True,
@@ -106,18 +118,23 @@ def create_content():
                         row_selectable="multi",
                         row_deletable=True,
                         selected_rows=[],
-                        style_cell={'textAlign': 'left'},
+                        style_cell={"textAlign": "left"},
                         style_as_list_view=False,
-                        style_cell_conditional=[{'if': {'row_index': 'odd'},
-                             'backgroundColor': 'rgb(248, 248, 248)'}],
-                        style_header={'backgroundColor': 'white',
-                        'fontWeight': 'bold'},
+                        style_cell_conditional=[
+                            {
+                                "if": {"row_index": "odd"},
+                                "backgroundColor": "rgb(248, 248, 248)",
+                            }
+                        ],
+                        style_header={"backgroundColor": "white", "fontWeight": "bold"},
                         pagination_mode="fe",
                         pagination_settings={
                             "displayed_pages": 1,
                             "current_page": 0,
-                            "page_size": 20},
-                        navigation="page"),
+                            "page_size": 20,
+                        },
+                        navigation="page",
+                    )
                 ]
             ),
             # 2. 2nd row: table
@@ -125,8 +142,10 @@ def create_content():
             html.Div(
                 children=[
                     dash_table.DataTable(
-                        id='datatable-interactivity3',
-                        columns=[{"name": i, "id": i, "deletable": True} for i in df1.columns],
+                        id="datatable-interactivity3",
+                        columns=[
+                            {"name": i, "id": i, "deletable": True} for i in df1.columns
+                        ],
                         data=df1.to_dict("rows"),
                         editable=True,
                         filtering=True,
@@ -135,27 +154,31 @@ def create_content():
                         row_selectable="multi",
                         row_deletable=True,
                         selected_rows=[],
-                        style_cell={'textAlign': 'left'},
+                        style_cell={"textAlign": "left"},
                         style_as_list_view=False,
-                        style_cell_conditional=[{'if': {'row_index': 'odd'},
-                             'backgroundColor': 'rgb(248, 248, 248)'}],
-                        style_header={'backgroundColor': 'white',
-                        'fontWeight': 'bold'},
+                        style_cell_conditional=[
+                            {
+                                "if": {"row_index": "odd"},
+                                "backgroundColor": "rgb(248, 248, 248)",
+                            }
+                        ],
+                        style_header={"backgroundColor": "white", "fontWeight": "bold"},
                         pagination_mode="fe",
                         pagination_settings={
                             "displayed_pages": 1,
                             "current_page": 0,
-                            "page_size": 20},
-                        navigation="page"),
+                            "page_size": 20,
+                        },
+                        navigation="page",
+                    )
                 ]
             ),
-
-
         ],
         id="content",
         style={"width": "100%", "height": "100%"},
     )
     return content
+
 
 #########################
 def create_footer():
@@ -183,6 +206,7 @@ def create_footer():
     footer = html.Footer(children=div, style=footer_style)
     return footer
 
+
 #########################
 def serve_layout():
     layout = html.Div(
@@ -200,7 +224,6 @@ for js in external_js:
     app.scripts.append_script({"external_url": js})
 for css in external_css:
     app.css.append_css({"external_url": css})
-
 
 
 if __name__ == "__main__":
