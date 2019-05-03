14f215b0d0ed28a579f801806dd1aaebc7e6f40a
dujm <jdu523@outlook.com>
2019-05-03 12:24:46 +0200

    reformat py

--- app/app.py
+++ app/app.py
@@ -2,68 +2,26 @@
 # Import packages
 ################################################################################
 import dash
-import dash_table
 from dash import Dash
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
 from dash_table_experiments import DataTable
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-import flask
 from flask import Flask, send_from_directory
 
 # Other
-import io
-import os
-import urllib
+import os, urllib
 import pandas as pd
-
-# For web scraping
-# import urllib
-import requests
-import bs4
-from bs4 import BeautifulSoup
-import re
-
 from dotenv import load_dotenv
-from exceptions import ImproperlyConfigured
-
 import chardet
-
 import subprocess
 
-pd.set_option("display.max_columns", None)
-###############################################################################
-def find_encoding(fname):
-    r_file = open(fname, 'rb').read()
-    result = chardet.detect(r_file)
-    charenc = result['encoding']
-    return charenc
-
-
-################################################################################
-# Call scrape function from scrape_de.py
-#from scrape_de import scrape_indeed_de
-################################################################################
-# Website information
-url_de = "http://de.indeed.com/jobs?q=data+scientist+&l={}&start={}"
-################################################################################
-# Variables
-My_City = set(["Cologne", "Berlin", "Munich", "Dusseldorf"])
-max_results_my_city = 5
-# Number of pages
-page = 2
-
-# Call function
-#scrape_indeed_de(url_de, My_City,max_results_my_city,page)
 
 ################################################################################
 # Setting
 ################################################################################
+pd.set_option("display.max_columns", None)
 if "DYNO" in os.environ:
     # the app is on Heroku
     debug = False
@@ -79,7 +37,7 @@ server = Flask(app_name, static_folder='static')
 app = Dash(name=app_name, server=server, csrf_protect=False)
 app.title = 'Smart Job Search'
 app.config.suppress_callback_exceptions = True
-app.scripts.config.serve_locally=True
+app.scripts.config.serve_locally = True
 
 external_js = []
 external_css = [
@@ -93,48 +51,64 @@ theme = {"font-family": "Lobster", "background-color": "#e0e0e0"}
 ################################################################################
 # Read Global AI data
 ################################################################################
-# Table 1 and 2
-df1_encoding = find_encoding("./data/Global-Artificial-Intelligence-Database-Asgard-2018.txt")
+# Table 1
+# Find data encoding
+def find_encoding(fname):
+    r_file = open(fname, 'rb').read()
+    result = chardet.detect(r_file)
+    charenc = result['encoding']
+    return charenc
+
+
+df1_encoding = find_encoding(
+    "./data/Global-Artificial-Intelligence-Database-Asgard-2018.txt"
+)
 df_health_encoding = find_encoding("./data/df_health.txt")
 
 
-df1 = pd.read_csv("./data/Global-Artificial-Intelligence-Database-Asgard-2018.csv",encoding="ISO-8859-1")
+df1 = pd.read_csv(
+    "./data/Global-Artificial-Intelligence-Database-Asgard-2018.csv",
+    encoding="ISO-8859-1",
+)
 df1.insert(0, 'Index', range(1, len(df1) + 1))
 
 # Table 2
-df_health = pd.read_csv("./data/df_health.csv",encoding="ISO-8859-1")
+df_health = pd.read_csv("./data/df_health.csv", encoding="ISO-8859-1")
 df_health.insert(0, 'Index', range(1, len(df_health) + 1))
 
 # Dictionary of two tables
-dataframes = {'Global AI Companies': df1,'Global Health AI Companies': df_health}
+dataframes = {'Global AI Companies': df1, 'Global Health AI Companies': df_health}
 print(dataframes.keys())
+
 
 def get_data_object(user_selection):
     """
     For user selections, return the relevant in-memory data frame.
     """
-    #return dataframes[user_selection]
+    # return dataframes[user_selection]
     return dataframes.get(user_selection)
+
+
 ################################################################################
 # Table 3 and 4
 de_encoding = find_encoding("./data/de_indeed.txt")
 
-df_indeed_de = pd.read_csv("./data/de_indeed.txt",sep="\t",encoding=de_encoding)
+df_indeed_de = pd.read_csv("./data/de_indeed.txt", sep="\t", encoding=de_encoding)
 df_indeed_de.insert(0, 'Index', range(1, len(df_indeed_de) + 1))
 print(df_indeed_de.head(2))
 
-dataframes_indeed = {'Germany': df_indeed_de
-
-}
-
+dataframes_indeed = {'Germany': df_indeed_de}
 print(dataframes_indeed.keys())
+
 
 def get_data_object2(user_selection2):
     """
     For user selections, return the relevant in-memory data frame.
     """
-    #return dataframes[user_selection]
+    # return dataframes[user_selection]
     return dataframes_indeed.get(user_selection2)
+
+
 ################################################################################
 # APP Functions and Layout
 ################################################################################
@@ -142,9 +116,12 @@ def create_header():
     header_style = {"background-color": theme["background-color"], "padding": "1.5rem"}
     header = html.Header(html.H1(children=app_name, style=header_style))
     return header
+
+
 ################################################################################
 def create_content():
-    content = html.Div(id='ai',
+    content = html.Div(
+        id='ai',
         children=[
             html.H2("Global AI companies"),
             html.Div(
@@ -178,36 +155,44 @@ def create_content():
             html.Div(
                 children=[
                     dcc.Dropdown(
-                    id='field-dropdown',
-                    options=[{'label': df, 'value': df} for df in dataframes],
-                    value="Global AI Companies"),
+                        id='field-dropdown',
+                        options=[{'label': df, 'value': df} for df in dataframes],
+                        value="Global AI Companies",
+                    ),
                     DataTable(
                         id='table',
-                    # rows
+                        # rows
                         rows=[{}],
-                        #columns=df_health.columns,
+                        # columns=df_health.columns,
                         row_selectable=True,
                         filterable=True,
                         sortable=True,
                         selected_row_indices=[],
                         max_rows_in_viewport=10,
-                        resizable =True,
+                        resizable=True,
                         enable_drag_and_drop=True,
                         header_row_height=50,
                         column_widths=200,
                         row_scroll_timeout=1,
-                        row_update= True,
-                        editable =True)]),
+                        row_update=True,
+                        editable=True,
+                    ),
+                ]
+            ),
             html.H2(),
             html.A(
                 'Download Data',
                 id='download-link',
                 download="global_ai_companies.csv",
                 href="",
-                target="_blank"
+                target="_blank",
             ),
-                    ])
+        ],
+    )
     return content
+
+
+###############################################################################
 def create_content2():
     content = html.Div(
         children=[
@@ -215,43 +200,52 @@ def create_content2():
             html.H2("Data Scientist Positions in Germany"),
             html.Div(
                 children=[
-                    html.Button("Search Indeed", id='button',n_clicks_timestamp=0),
-                    html.Div(id='button-container')
-                ]),
+                    html.Button("Search Indeed", id='button', n_clicks_timestamp=0),
+                    html.Div(id='button-container'),
+                ]
+            ),
             html.Div(
                 children=[
                     dcc.Dropdown(
-                    id='field-dropdown2',
-                    options=[{'label': df, 'value': df} for df in dataframes_indeed],
-                    value="Germany"),
+                        id='field-dropdown2',
+                        options=[
+                            {'label': df, 'value': df} for df in dataframes_indeed
+                        ],
+                        value="Germany",
+                    ),
                     DataTable(
                         id='table_indeed',
                         rows=[{}],
-                        #columns=df_health.columns,
+                        # columns=df_health.columns,
                         row_selectable=True,
                         filterable=True,
                         sortable=True,
                         selected_row_indices=[],
                         max_rows_in_viewport=10,
-                        resizable =True,
+                        resizable=True,
                         enable_drag_and_drop=True,
                         header_row_height=50,
                         column_widths=200,
                         row_scroll_timeout=1,
-                        row_update= True,
-                        editable =True)]),
+                        row_update=True,
+                        editable=True,
+                    ),
+                ]
+            ),
             html.H2(),
             html.A(
                 'Download Data Scientist Jobs',
                 id='download-link2',
                 download="data_scientist_position.txt",
                 href="",
-                target="_blank"
+                target="_blank",
             ),
             html.Hr(),
-
-    ])
+        ]
+    )
     return content
+
+
 ################################################################################
 # Footer
 def create_footer():
@@ -270,16 +264,14 @@ def create_footer():
             html.H2(),
             html.Span("Global AI Company Data from "),
             html.A(
-                "https://asgard.vc/global-ai/",
-                href="https://some-website.com/",
+                "https://asgard.vc/global-ai",
+                href="https://asgard.vc/global-ai/",
                 target="_blank",
             ),
             html.H2(),
             html.Span("Job data from "),
             html.A(
-                "https://de.indeed.com/",
-                href="https://de.indeed.com/",
-                target="_blank",
+                "https://de.indeed.com", href="https://de.indeed.com/", target="_blank"
             ),
         ]
     )
@@ -287,15 +279,23 @@ def create_footer():
     div = html.Div([p0, p1])
     footer = html.Footer(children=div, style=footer_style)
     return footer
+
+
 ################################################################################
 # Layout
 def serve_layout():
     layout = html.Div(
-        children=[create_header(), create_content(), create_content2(), create_footer()],
+        children=[
+            create_header(),
+            create_content(),
+            create_content2(),
+            create_footer(),
+        ],
         className="container",
         style={"font-family": theme["font-family"]},
     )
     return layout
+
 
 ################################################################################
 # Callbacks
@@ -305,7 +305,6 @@ for js in external_js:
     app.scripts.append_script({"external_url": js})
 for css in external_css:
     app.css.append_css({"external_url": css})
-################################################################################
 ################################################################################
 # Display table
 @app.callback(Output('table', 'rows'), [Input('field-dropdown', 'value')])
@@ -320,25 +319,41 @@ def update_table(user_selection):
         return df
     else:
         print('Data is not selected yet')
+
+
 ################################################################################
 # Download table link
 @app.callback(
     dash.dependencies.Output('download-link', 'href'),
-    [dash.dependencies.Input('table', 'rows')])
+    [dash.dependencies.Input('table', 'rows')],
+)
 def update_download_link(rows):
     dff = pd.DataFrame(rows)
-    txt_string = dff.to_csv(index=False,
-        header = True,
-        columns=["Index","Country","State","City","Name","Category","Description","Website"],
-        encoding="ISO-8859-1"
-        )
+    txt_string = dff.to_csv(
+        index=False,
+        header=True,
+        columns=[
+            "Index",
+            "Country",
+            "State",
+            "City",
+            "Name",
+            "Category",
+            "Description",
+            "Website",
+        ],
+        encoding="ISO-8859-1",
+    )
     txt_string = "data:text/csv;charset=ISO-8859-1," + urllib.parse.quote(txt_string)
     return txt_string
-################################################################################
+
+
 ################################################################################
 # Display ds table
 @app.callback(
-    dash.dependencies.Output('table_indeed', 'rows'), [dash.dependencies.Input('field-dropdown2', 'value')])
+    dash.dependencies.Output('table_indeed', 'rows'),
+    [dash.dependencies.Input('field-dropdown2', 'value')],
+)
 def update_table2(user_selection2):
     """
     For user selections, return the relevant table
@@ -351,45 +366,51 @@ def update_table2(user_selection2):
     else:
         print('Indeed data is not selected yet')
 
-# Download table link
 
+################################################################################
+# Download table link
 @app.callback(
     dash.dependencies.Output('download-link2', 'href'),
-    [dash.dependencies.Input('table_indeed', 'rows')])
+    [dash.dependencies.Input('table_indeed', 'rows')],
+)
 def update_download_link(rows):
     dff2 = pd.DataFrame(rows)
-    txt_string2 = dff2.to_csv(index=False,
-        header = True,
-        sep="\t",
-        )
+    txt_string2 = dff2.to_csv(index=False, header=True, sep="\t")
     # ignore encoding here
     txt_string2 = "data:text/csv;charset=de_encoding," + urllib.parse.quote(txt_string2)
     return txt_string2
+
+
 ################################################################################
 # Click button and scrape data on indeed
 @app.callback(
     dash.dependencies.Output('button-container', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')])
+    [dash.dependencies.Input('button', 'n_clicks')],
+)
 def displayClick(n_clicks):
     if n_clicks is None:
-        msg ='Using pre-downloaded data from Indeed'
+        msg = 'Using pre-downloaded data from Indeed'
     else:
         print('search indeed.de')
         subprocess.call(" python scrape_de.py 1", shell=True)
         print('finished searching')
-        #os.system('scrape_de.py 1')
+        # os.system('scrape_de.py 1')
         msg = 'Download the latest data from Indeed'
 
     return html.Div(msg)
+
+
 ############################################################################### # facicon
 @app.server.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(server.root_path, 'static'),
-    'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
+    return send_from_directory(
+        os.path.join(server.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon',
+    )
 
 
 ################################################################################
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run_server(port=port, threaded=True, debug=True) #debug=debug,
+    app.run_server(port=port, threaded=True, debug=True)  # debug=debug,
--- app/scrape_de.py
+++ app/scrape_de.py
@@ -1,20 +1,17 @@
-import urllib
-import requests
-import bs4
+import os, requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
-import time, os
-################################################################################
 
+################################################################################
 # The scraping function
-def scrape_indeed_de(url_template, Cities,max_results_per_city,i):
+################################################################################
+def scrape_indeed_de(url_template, Cities, max_results_per_city, i):
     df_de = pd.DataFrame(columns=["Title", "Location", "Company", "Review", "Summary"])
     for city in Cities:
         for start in range(0, max_results_per_city, 1):
-        # Grab the results from the request (as above)
+            # Grab the results from the request (as above)
             url = url_template.format(city, start)
-        # Append to the full set of results
+            # Append to the full set of results
             html = requests.get(url)
             soup = BeautifulSoup(html.content, "html.parser", from_encoding="UTF-8")
             for each in soup.find_all(class_="result"):
@@ -23,7 +20,9 @@ def scrape_indeed_de(url_template, Cities,max_results_per_city,i):
                 except:
                     title = None
                 try:
-                    location = each.find("span", {"class": "location"}).text.replace("\n", "")
+                    location = each.find("span", {"class": "location"}).text.replace(
+                        "\n", ""
+                    )
                 except:
                     location = None
                 try:
@@ -51,30 +50,32 @@ def scrape_indeed_de(url_template, Cities,max_results_per_city,i):
                     ignore_index=True,
                 )
                 i += 1
-                if (
-                    i % 1000 == 0
-                ):  # Ram helped me build this counter to see how many. You can visibly see Ram's vernacular in the print statements.
+                if i % 1000 == 0:
                     print(
                         "You have "
                         + str(i)
                         + " results. "
                         + str(df_de.dropna().drop_duplicates().shape[0])
-                        + " of these are unique results")
+                        + " of these are unique results"
+                    )
 
-# csv_name = time.strftime('%Y%m%d-%H-%M%S') + '_de_'+ 'indeed.csv'
+    # csv_name = time.strftime('%Y%m%d-%H-%M%S') + '_de_'+ 'indeed.csv'
     txt_name = "de_" + "indeed.txt"
-    df_de.to_csv(os.path.join("./data/", txt_name),sep="\t",index=False)
+    df_de.to_csv(os.path.join("./data/", txt_name), sep="\t", index=False)
     return df_de.head(2)
+
+
 ################################################################################
 # Variables
+################################################################################
 # Germany
 url_de = "http://de.indeed.com/jobs?q=data+scientist+&l={}&start={}"
-#My_City = set(["Köln", "Berlin", "München", "Düsseldorf"])
+# My_City = set(["Köln", "Berlin", "München", "Düsseldorf"])
 My_City = set(["Cologne", "Berlin", "Munich", "Dusseldorf"])
 max_results_my_city = 20
 # Number of pages
 page = 2
 
 # Call function
-scrape_indeed_de(url_de, My_City,max_results_my_city,page)
+scrape_indeed_de(url_de, My_City, max_results_my_city, page)
 ################################################################################
